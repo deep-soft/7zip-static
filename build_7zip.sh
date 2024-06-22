58b06d43706173400249aa0484916bcb96d13b75
spvkgn <spvkgn@users.noreply.github.com>
2024-06-22 14:30:16 +0500

    update patch

--- build_7zip.sh
+++ build_7zip.sh
@@ -21,14 +21,14 @@ case $ARCH in
     MAKE_OPTS="-f ../../cmpl_gcc.mak";;
 esac
 
-git ls-files -z | xargs -0 unix2dos -q --allow-chown && QUILT_PATCHES=../patches quilt push -a && \
+git ls-files -z | xargs -0 unix2dos -q --allow-chown && (QUILT_PATCHES=../patches quilt push -a || exit 1)
 
-cd CPP/7zip/Bundles/Alone2 && mkdir -p b/g && \
-make -j$(nproc) \
-  CC="ccache gcc" CXX="ccache g++" \
-  CFLAGS_BASE_LIST="-c -D_7ZIP_AFFINITY_DISABLE=1 -DZ7_AFFINITY_DISABLE=1 -D_GNU_SOURCE=1" \
-  CFLAGS_WARN_WALL="-Wall -Wextra" COMPL_STATIC=1 $MAKE_OPTS && \
+( cd CPP/7zip/Bundles/Alone2 && mkdir -p b/g && \
+  make -j$(nproc) \
+    CC="ccache gcc" CXX="ccache g++" \
+    CFLAGS_BASE_LIST="-c -D_7ZIP_AFFINITY_DISABLE=1 -DZ7_AFFINITY_DISABLE=1 -D_GNU_SOURCE=1" \
+    CFLAGS_WARN_WALL="-Wall -Wextra" COMPL_STATIC=1 $MAKE_OPTS || exit 1 )
 
-find . -type f -name '7zzs' -exec cp -va {} 7zz \; && tar -cJvf ../7zz-linux-$PLATFORM.tar.xz 7zz
+find . -type f -name '7zzs' -exec cp -va {} 7zz \; && tar -cJvf $GITHUB_WORKSPACE/7zz-linux-$PLATFORM.tar.xz 7zz
 
 ccache --max-size=50M --show-stats
