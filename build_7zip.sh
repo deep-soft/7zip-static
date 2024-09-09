21729e4d5e3ec42c9dedd568d4e7fc0b97b996b6
spvkgn <spvkgn@users.noreply.github.com>
2024-09-09 11:16:32 +0500

    Use musl-cross toolchain

--- build_7zip.sh
+++ build_7zip.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 set -eo pipefail
 
-export CCACHE_DIR="$GITHUB_WORKSPACE/.ccache"
-
 case $ARCH in
-  x86_64)
-    PLATFORM=x64
-    MAKE_OPTS="MY_ASM=uasm -f ../../cmpl_gcc_$PLATFORM.mak";;
-  x86)
-    PLATFORM=x86
-    MAKE_OPTS="MY_ASM=uasm -f ../../cmpl_gcc_$PLATFORM.mak";;
-  aarch64)
-    PLATFORM=arm64
-    MAKE_OPTS="MY_ASM=uasm -f ../../cmpl_gcc_$PLATFORM.mak";;
-  armhf)
-    PLATFORM=arm
-    MAKE_OPTS="-f ../../cmpl_gcc_$PLATFORM.mak";;
+  x86-64)
+    MAKE_OPTS="MY_ASM=uasm -f ../../cmpl_gcc_x64.mak";;
+  x86|arm64)
+    MAKE_OPTS="MY_ASM=uasm -f ../../cmpl_gcc_$ARCH.mak";;
+  arm|armhf)
+    MAKE_OPTS="-f ../../cmpl_gcc_arm.mak";;
   *)
-    PLATFORM=$ARCH
     MAKE_OPTS="-f ../../cmpl_gcc.mak";;
 esac
 
@@ -25,10 +16,8 @@ git ls-files -z | xargs -0 unix2dos -q --allow-chown && (QUILT_PATCHES=../patche
 
 ( cd CPP/7zip/Bundles/Alone2 && mkdir -p b/g && \
   make -j$(nproc) \
-    CC="ccache gcc" CXX="ccache g++" \
-    CFLAGS_BASE_LIST="-c -D_7ZIP_AFFINITY_DISABLE=1 -DZ7_AFFINITY_DISABLE=1 -D_GNU_SOURCE=1" \
+    CROSS_COMPILE="${TOOL}-" \
+    CFLAGS_BASE_LIST="$CFLAGS_EXTRA -c -D_7ZIP_AFFINITY_DISABLE=1 -DZ7_AFFINITY_DISABLE=1 -D_GNU_SOURCE=1" \
     CFLAGS_WARN_WALL="-Wall -Wextra" COMPL_STATIC=1 $MAKE_OPTS || exit 1 )
 
-find . -type f -name '7zzs' -exec cp -va {} 7zz \; && tar -cJvf $GITHUB_WORKSPACE/7zz-linux-$PLATFORM.tar.xz 7zz
-
-ccache --max-size=50M --show-stats
+find . -type f -name '7zzs' -exec cp -va {} 7zz \; && tar -cJvf $GITHUB_WORKSPACE/7zz-linux-$ARCH.tar.xz 7zz
